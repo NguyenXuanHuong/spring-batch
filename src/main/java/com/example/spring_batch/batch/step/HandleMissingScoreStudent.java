245d6339025a593c8db76615999d2a367159853b
xuanhuong <xuanhuong@gmail.com>
2024-09-08 10:35:02 +0900

    resolve log

--- src/main/java/com/example/spring_batch/batch/step/HandleMissingScoreStudent.java
+++ src/main/java/com/example/spring_batch/batch/step/HandleMissingScoreStudent.java
@@ -1,8 +1,11 @@
 package com.example.spring_batch.batch.step;
 
+import com.example.spring_batch.batch.processor.StudentScoreProcessor;
 import com.example.spring_batch.batch.repository.StudentScoreRepository;
 import com.example.spring_batch.batch.repository.Top3StudentRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -15,6 +18,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 @Slf4j
 public class HandleMissingScoreStudent {
+    private static final Logger log = LogManager.getLogger(HandleMissingScoreStudent.class);
+
     private final JobRepository jobRepository;
     private final PlatformTransactionManager txm;
     private final StudentScoreRepository studentScoreRepository;
