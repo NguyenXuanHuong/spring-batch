f24caa4ee00445a5d621e92dc8177db8c1e7f131
xuanhuong <xuanhuong@gmail.com>
2024-09-19 22:16:45 +0900

    add logic to get job parameter

--- src/main/java/com/example/spring_batch/batch/processor/StudentScoreProcessor.java
+++ src/main/java/com/example/spring_batch/batch/processor/StudentScoreProcessor.java
@@ -5,16 +5,28 @@ import com.example.spring_batch.batch.entity.StudentScoreEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@StepScope
 public class StudentScoreProcessor implements ItemProcessor<StudentScoreDto, StudentScoreEntity> {
     private static final Logger log = LogManager.getLogger(StudentScoreProcessor.class);
 
+    @Value("#{stepExecution}")
+    private StepExecution stepExecution;
+
     @Override
     public StudentScoreEntity process(StudentScoreDto studentScoreDto){
+
+        String parameterValue = stepExecution.getJobParameters().getString("job-run-date");
+        System.out.println(parameterValue);
+
         StudentScoreEntity studentScoreEntity = new StudentScoreEntity();
         studentScoreEntity.setScore(studentScoreDto.getScore());
         studentScoreEntity.setAge(studentScoreDto.getAge());
